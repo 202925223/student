8ef3bc49ead40f4329480ff87b7ec8ff6538e66f
202925223 <202925223@qq.com>
2014-11-09 21:12:24 +0800

    Update CheckStu.cpp

--- CheckStu.cpp
+++ CheckStu.cpp
@@ -2,7 +2,6 @@
 
 void CheckStu(stusystem *sys)
 {
-	int i = 0;
 	student *temp = NULL;
 	
 	if (!sys->head)
@@ -14,7 +13,7 @@ void CheckStu(stusystem *sys)
 	temp = sys->head;
 	while (temp)
 	{
-		printf("\n\t学生[%d]: %s, 名字: %s\n", i++, temp->id, temp->name);
+		printf("\n\t学生[%s], 名字: %s\n", temp->id, temp->name);
 		temp = temp->next;
 	}
 	printf("\n\t到达学生信息表底部！\n\n");
