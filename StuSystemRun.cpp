044f8246eb4410fd9b6faa0d171e5dbffb7ef47a
202925223 <202925223@qq.com>
2014-11-09 21:14:59 +0800

    Update StuSystemRun.cpp

--- StuSystemRun.cpp
+++ StuSystemRun.cpp
@@ -5,26 +5,24 @@ void StuSystemRun(stusystem *sys)
 	char c;
 	while (c = menu())
 	{
-		if (c == '0')
-		{
-			printf("你选择了退出系统\n");
-			break;
-		}
 		switch (c)
 		{
-		case '1':
-			printf("开始学生信息输入模块...\n");
-			InputStu(sys);
-			printf("结束学生信息输入模块...\n");
-			break;
-		case '2':
-			printf("开始学生信息查询模块...\n");
-			CheckStu(sys);
-			printf("结束学生信息查询模块...\n");
-			break;
-		default:
-			printf("没有此菜单，请重新选择一项：\n");
-			break;
+			case '0':
+				printf("你选择了退出系统\n");
+				return;
+			case '1':
+				printf("开始学生信息输入模块...\n");
+				InputStu(sys);
+				printf("结束学生信息输入模块...\n");
+				break;
+			case '2':
+				printf("开始学生信息查询模块...\n");
+				CheckStu(sys);
+				printf("结束学生信息查询模块...\n");
+				break;
+			default:
+				printf("没有此菜单，请重新选择一项：\n");
+				break;
 		}
 	}
 }
